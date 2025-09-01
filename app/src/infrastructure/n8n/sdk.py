31ab42f8096f4901fc7914d7e823f387d15e46e7
Ariana Maghsoudi <ariana.maghsoudi82@gmail.com>
2025-09-01 08:00:44 +0330

    fix: Correct n8n sdk and clinet encapsulation

--- app/src/infrastructure/n8n/sdk.py
+++ app/src/infrastructure/n8n/sdk.py
@@ -8,10 +8,10 @@ from app.src.user_case.n8n_client import N8nClientInterface
 class N8nSDK(Encapsulated, Immutable):
     # TODO: Implement authentication
     def __init__(self, base_url: str) -> None:
-        self.base_url = base_url
+        self.__base_url = base_url
 
     def post_webhook(self, path: str, body: dict[str, Any])->dict[str, Any]:
-        response = requests.post(f"{self.base_url}/{path}", json=body)
+        response = requests.post(f"{self.__base_url}/{path}", json=body)
         if response.status_code != requests.codes.ok:
             # TODO: Make custom error classes
             raise RuntimeError('Failed to post webhook', response.json())
@@ -19,11 +19,11 @@ class N8nSDK(Encapsulated, Immutable):
 
 class N8nClient(N8nClientInterface, Encapsulated, Immutable):
     def __init__(self, sdk: N8nSDK, code_reviewer_webhook_path: str, default_version: str= 'v1'):
-        self.sdk = sdk
-        self.code_reviewer_webhook_path = code_reviewer_webhook_path
-        self.default_version = default_version
+        self.__sdk = sdk
+        self.__code_reviewer_webhook_path = code_reviewer_webhook_path
+        self.__default_version = default_version
 
-    def request_review(self, data: dict[str, Any], version: str|None) -> dict[str, Any]:
+    def request_review(self, data: dict[str, Any], version: str|None=None) -> dict[str, Any]:
         if version is None:
-            version = self.default_version
-        return self.sdk.post_webhook(f'{self.code_reviewer_webhook_path}/{version}', data)
+            version = self.__default_version
+        return self.__sdk.post_webhook(f'{self.__code_reviewer_webhook_path}/{version}', data)
